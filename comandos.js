f012a2539f970249d865160a53524cc60ef4d40a
sabayarw <sabayarw@gmail.com>
2022-10-18 18:36:29 -0300

    agregando comandos

---
+++ comandos.js
@@ -0,0 +1,7 @@
+/*
+1 Para añadir todos los cambios: git add .
+2 Para confirmar los cambios: git commit -m "texto descriptivo del cambio"
+3 Para elevar al repositorio remoto en github: git push
+4 Para saber el estado de los archivos: git status
+el 4 se puede hacer varias veces cuando quieras
+*/
