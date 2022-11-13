7d0e0916db58daeb9661f6a8122abdd2638ced7b
sabayarw <sabayarw@gmail.com>
2022-11-13 20:44:03 -0300

    agregando más comandos

--- comandos.js
+++ comandos.js
@@ -4,4 +4,10 @@
 3 Para elevar al repositorio remoto en github: git push
 4 Para saber el estado de los archivos: git status
 el 4 se puede hacer varias veces cuando quieras
+para limpiar: git clear
+Para añadir estilos css en html con la etiqueta: <style>
+para comentar primero /* y al final * /
+clrt k n : para arbir nuevo archivo, elegir el lenguaje y crtl s para guardar con nombre
+
 */
+
