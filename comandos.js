f907304a3059fecec8f74391e7df502808aa2f11
sabayarw <sabayarw@gmail.com>
2022-12-28 16:29:04 -0300

    x

---
+++ comandos.js
@@ -0,0 +1,33 @@
+/*
+1 Para añadir todos los cambios: git add .
+2 Para confirmar los cambios: git commit -m "texto descriptivo del cambio"
+3 Para elevar al repositorio remoto en github: git push
+4 Para saber el estado de los archivos: git status
+el 4 se puede hacer varias veces cuando quieras
+para limpiar: git clear
+Para añadir estilos css en html con la etiqueta: <style>
+para comentar primero /* y al final * /
+clrt k n : para arbir nuevo archivo, elegir el lenguaje y crtl s para guardar con nombre
+HTML:
+<x> etiqueta de apertura
+</x> etiqueta de cierre
+x es el contenido de la etiqueta html
+h1 es etiqueta de titulo, luego h2, h3, etc... (bajan en tamaños)
+CSS:
+Para colocar una regla hacer : y el valor finalizando con ;
+Por regla general se usa en JS el nombre del archivo en camel case (mayuscula para separar palabaras)
+hr es salto de linea (espacio entre textos con una linea)
+pr salto de linea (sin espacio)
+etiqueta <strong> para negrita
+shift y delete para borrar la linea entera
+<div> etiqueta para dividir elementos
+etiqueta >span> es para que solo lo que escribas detnro del span se cambie de background o la letra de color, etc. el atributo
+etiqueta <a para link
+el target_blank es para que el enlace se abra en otra pantalla
+para clonar es: alt + shift + flecha para abajo o arriba
+en html la eitqueta lang es para colocar el lenguaje: <html lang="es" o "en"
+shift+delete para borrar la linea entera
+
+Las clases de CSS se separan con un espacio class= "hola menuNuevo"
+*/
+
